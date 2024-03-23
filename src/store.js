1dec08629269b1433d0c6b02d5627aebc5418e15
ometman <revomet@gmail.com>
2024-03-23 06:30:01 +0000

    Modify store.jsx for authentication fix

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
-import rootReducer from './reducers';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'; // Assuming you have a rootReducer that combines reducers
+import authReducer from './authSlice';
 
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  reducer: {
+    root: rootReducer,
+    auth: authReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
 
 export default store;
